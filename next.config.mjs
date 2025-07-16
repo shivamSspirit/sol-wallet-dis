d841515218b7c974facd50712607b60092ae383d
shivam <shivam.spirite3@outlook.com>
2025-07-17 02:42:05 +0530

    remove vercel config and upadate it

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,11 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
   typescript: {
     ignoreBuildErrors: true
   },
-
   images: {
     domains: ['res.cloudinary.com'],
-    // OR use remotePatterns for more control (recommended)
     remotePatterns: [
       {
         protocol: 'https',
